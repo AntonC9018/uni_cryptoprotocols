aceee3fa2f79727669a7db093d9645ba50b16fb0
AntonC9018 <anton.curm@gmail.com>
2021-09-27 22:35:00 +0300

    tag done

--- source/app.d
+++ source/app.d
@@ -2,57 +2,31 @@ import std.digest;
 import std.digest.sha;
 import std.range;
 import std.string;
+import std.algorithm;
 
 void main()
 {
-    import aes;
-    import std.algorithm.comparison;
-
-
-
-
-    enum keyBitSize = 128;
-    ubyte[keyBitSize/8] key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
-    aes_context ctx;
-    aes_setkey_enc(&ctx, key.ptr, keyBitSize); 
-
-    enum inputSize = 16;
-    auto doAes(ref const(ubyte)[inputSize] input)
-    {
-        ubyte[inputSize] output;
-        aes_crypt_ecb(&ctx, false, input.ptr, output.ptr);
-        return output;
-    }
-
-    ubyte[inputSize] zeros = 0;
-    import std.stdio;
-    assert(doAes(zeros).toHexString!(LetterCase.lower) == "7df76b0c1ab899b33e42f047b91b546f");
-    auto processedKey = getKeyCMAC!16(&doAes, key[]);
-    writeln(processedKey.k1.toHexString!(LetterCase.lower));
-    writeln(processedKey.k2.toHexString!(LetterCase.lower));
-    auto tag = getTagCMAC!16(&doAes, processedKey, []);
-
-    writeln(tag.toHexString!(LetterCase.lower));
-    
-    // // ubyte[keyBitSize/8] key = 0;
-    // // string keyString = "key";
-    // // foreach (i; 0..min(keyString.length, keyBitSize/8))
-    // //     key[i] = cast(ubyte) keyString[i];
-
-    // ubyte[keyBitSize/8] key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
-    
-    // // ubyte[16] input = 0;
-    // // string inputString = "The quick brown fox jumped over the lazy dog";
-    // // foreach (i; 0..min(inputSize, inputSize))
-    // //     input[i] = inputString[i];
-    // ubyte[16] input = [0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a];
-
-    // ubyte[16] output = 0;
-    // aes_crypt_ecb(&ctx, false, input.ptr, output.ptr);
-
-    // import std.stdio;
-    // writeln(output.toHexString!(LetterCase.lower));
 }
+
+
+auto fromHexString(string source)
+{
+    import std.utf;
+    import std.conv : to;
+    return source.byCodeUnit.chunks(2).map!(c => c.to!ubyte(16));
+}
+unittest
+{
+    assert(equal(fromHexString("1234ABcd"), [0x12, 0x34, 0xab, 0xcd]));
+}
+
+void fromHexString(ubyte[] output, string source)
+{
+    assert(output.length == source.length / 2);
+    foreach (i, b; fromHexString(source).enumerate)
+        output[i] = b;
+}
+
 
 auto calculateHMAC(alias hashFunction, int blockSize)(const(ubyte)[] key, const(ubyte)[] message)
 {
@@ -82,23 +56,6 @@ unittest
     assert(hmac.toHexString!(LetterCase.lower) == "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8");
 }
 
-void shiftLeftInto(ubyte[] arrSource, ubyte[] arrDest)
-{
-    ubyte leftover = 0;
-    foreach (i; 0..arrSource.length)
-    {
-        arrDest[i] = cast(ubyte)(arrSource[i] << 1) | leftover;
-        leftover = (arrSource[i] & 0x80) >> 7; 
-    }
-}
-unittest
-{
-    ubyte[4] source = [0x80, 0x01, 0x88, 0x00];
-    ubyte[4] dest = void;
-    shiftLeftInto(source[], dest[]);
-    assert(dest[] == [0x00, 0x03, 0x10, 0x01]);
-}
-
 
 struct KeyCMAC(int blockSize)
 {
@@ -124,44 +81,106 @@ auto getKeyCMAC(int blockSize)(scope BlockCipher!blockSize blockCipher, const(ub
 
     ubyte[blockSize] k1 = void;
     shiftLeftInto(k0[], k1[]);
-    if (k0[$-1] & 0x80)
-        k1[] ^= c;
+    if (k0[0] & 0x80)
+        k1[15] ^= c;
 
     ubyte[blockSize] k2 = void;
     shiftLeftInto(k1[], k2[]);
-    if (k1[$-1] & 0x80)
-        k2[] ^= c;
+    if (k1[0] & 0x80)
+        k2[15] ^= c;
 
     return KeyCMAC!blockSize(k1, k2);
 }
 
-auto getTagCMAC(int blockSize)(scope BlockCipher!blockSize blockCipher, KeyCMAC!blockSize key, const(ubyte)[] message)
+auto getTagCMAC(int blockSize)(scope BlockCipher!blockSize blockCipher, 
+    ref const(KeyCMAC!blockSize) key, const(ubyte)[] message)
 {
     ubyte[blockSize] mLast = void;
+    auto numIters = message.length / blockSize;
+
     // complete block
     if (message.length && message.length % blockSize == 0)
-        mLast[] = key.k1[] ^ message[$ - blockSize..$];
+    {
+        mLast[0..$] = key.k1[] ^ message[$ - blockSize..$];
+        numIters--;
+    }
     // incomplete block
     else
     {
         auto byteCount = message.length % blockSize;
         mLast[0..byteCount]     = key.k2[0..byteCount] ^ message[$ - byteCount..$];
-        mLast[byteCount]        = key.k2[byteCount] ^ 1;
+        mLast[byteCount]        = key.k2[byteCount] ^ 0x80;
         mLast[byteCount + 1..$] = key.k2[byteCount + 1..$];
     }
     
     size_t messageIndex = 0;
     ubyte[blockSize] x = 0;
-    ubyte[blockSize] y = void;
 
-    auto numIters = message.length / blockSize;
     foreach (i; 0..numIters)
     {
-        y[] = x[] ^ message[messageIndex..messageIndex + blockSize];
-        x = blockCipher(y);
+        x[] ^= message[messageIndex..messageIndex + blockSize];
+        x = blockCipher(x);
         messageIndex += blockSize;
     }
 
-    y[] = mLast[] ^ x[];
-    return blockCipher(y);
+    x[] ^= mLast[];
+    return blockCipher(x);
+}
+unittest
+{
+    import aes;
+
+    // Test vectors: https://datatracker.ietf.org/doc/html/rfc4493#section-4
+    enum keyBitSize = 128;
+    ubyte[keyBitSize/8] key = void;
+    fromHexString(key[], "2b7e151628aed2a6abf7158809cf4f3c");
+
+    aes_context ctx;
+    aes_setkey_enc(&ctx, key.ptr, keyBitSize); 
+
+    enum inputSize = 16;
+    auto doAes(ref const(ubyte)[inputSize] input)
+    {
+        ubyte[inputSize] output;
+        aes_crypt_ecb(&ctx, false, input.ptr, output.ptr);
+        return output;
+    }
+
+    ubyte[inputSize] zeros = 0;
+    assert(doAes(zeros).toHexString!(LetterCase.lower) == "7df76b0c1ab899b33e42f047b91b546f");
+
+    auto processedKey = getKeyCMAC!16(&doAes, key[]);
+    assert(processedKey.k1.toHexString!(LetterCase.lower) == "fbeed618357133667c85e08f7236a8de");
+    assert(processedKey.k2.toHexString!(LetterCase.lower) == "f7ddac306ae266ccf90bc11ee46d513b");
+
+    ubyte[inputSize] tag;
+    
+    tag = getTagCMAC!16(&doAes, processedKey, []);
+    assert(equal(tag[], fromHexString("bb1d6929e95937287fa37d129b756746")));
+
+    tag = getTagCMAC!16(&doAes, processedKey, fromHexString("6bc1bee22e409f96e93d7e117393172a").array);
+    assert(equal(tag[], fromHexString("070a16b46b4d4144f79bdd9dd04a287c")));
+
+    tag = getTagCMAC!16(&doAes, processedKey, 
+        fromHexString("6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411").array);
+    assert(equal(tag[], fromHexString("dfa66747de9ae63030ca32611497c827")));
+}
+
+
+void shiftLeftInto(ubyte[] arrSource, ubyte[] arrDest)
+{
+    ubyte leftover = 0;
+    auto len = arrSource.length;
+    foreach (i; iota(len).retro)
+    {
+        arrDest[i] = cast(ubyte)((arrSource[i] << 1) | leftover);
+        leftover = (arrSource[i] & 0x80) >> 7; 
+    }
+}
+unittest
+{
+    ubyte[4] source = [0x00, 0x81, 0x88, 0x80];
+    ubyte[4] dest = void;
+    shiftLeftInto(source[], dest[]);
+    assert(dest[] == [0x01, 0x03, 0x11, 0x00]);
 }
