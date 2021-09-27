22a255989d8c0ee1965d1883fa109d542b36859f
AntonC9018 <anton.curm@gmail.com>
2021-09-27 14:53:09 +0300

    stuff

--- source/app.d
+++ source/app.d
@@ -5,6 +5,53 @@ import std.string;
 
 void main()
 {
+    import aes;
+    import std.algorithm.comparison;
+
+
+
+
+    enum keyBitSize = 128;
+    ubyte[keyBitSize/8] key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
+    aes_context ctx;
+    aes_setkey_enc(&ctx, key.ptr, keyBitSize); 
+
+    enum inputSize = 16;
+    auto doAes(ref const(ubyte)[inputSize] input)
+    {
+        ubyte[inputSize] output;
+        aes_crypt_ecb(&ctx, false, input.ptr, output.ptr);
+        return output;
+    }
+
+    ubyte[inputSize] zeros = 0;
+    import std.stdio;
+    assert(doAes(zeros).toHexString!(LetterCase.lower) == "7df76b0c1ab899b33e42f047b91b546f");
+    auto processedKey = getKeyCMAC!16(&doAes, key[]);
+    writeln(processedKey.k1.toHexString!(LetterCase.lower));
+    writeln(processedKey.k2.toHexString!(LetterCase.lower));
+    auto tag = getTagCMAC!16(&doAes, processedKey, []);
+
+    writeln(tag.toHexString!(LetterCase.lower));
+    
+    // // ubyte[keyBitSize/8] key = 0;
+    // // string keyString = "key";
+    // // foreach (i; 0..min(keyString.length, keyBitSize/8))
+    // //     key[i] = cast(ubyte) keyString[i];
+
+    // ubyte[keyBitSize/8] key = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
+    
+    // // ubyte[16] input = 0;
+    // // string inputString = "The quick brown fox jumped over the lazy dog";
+    // // foreach (i; 0..min(inputSize, inputSize))
+    // //     input[i] = inputString[i];
+    // ubyte[16] input = [0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a];
+
+    // ubyte[16] output = 0;
+    // aes_crypt_ecb(&ctx, false, input.ptr, output.ptr);
+
+    // import std.stdio;
+    // writeln(output.toHexString!(LetterCase.lower));
 }
 
 auto calculateHMAC(alias hashFunction, int blockSize)(const(ubyte)[] key, const(ubyte)[] message)
@@ -55,34 +102,44 @@ unittest
 
 struct KeyCMAC(int blockSize)
 {
-    const(ubyte)[] k;
     ubyte[blockSize] k1;
     ubyte[blockSize] k2;
 }
 
-auto getKeyCMAC(alias blockCipher, int blockSize, const(ubyte)[] c)(const(ubyte)[] key)
+template BlockCipher(int blockSize)
 {
+    alias BlockCipher = ubyte[blockSize] delegate(ref const(ubyte)[blockSize]);
+}
+
+auto getKeyCMAC(int blockSize)(scope BlockCipher!blockSize blockCipher, const(ubyte)[] key)
+{
+    static if (blockSize == 8)
+        enum ubyte c = 0x1b;
+    else static if (blockSize == 16)
+        enum ubyte c = 0x87;
+    else static assert(0);
+
     ubyte[blockSize] zeros = 0;
-    auto k0 = blockCipher(key, zeros[]);
+    auto k0 = blockCipher(zeros);
 
     ubyte[blockSize] k1 = void;
     shiftLeftInto(k0[], k1[]);
     if (k0[$-1] & 0x80)
-        k1[] ^= c[];
+        k1[] ^= c;
 
     ubyte[blockSize] k2 = void;
     shiftLeftInto(k1[], k2[]);
     if (k1[$-1] & 0x80)
-        k2[] ^= c[];
+        k2[] ^= c;
 
-    return KeyCMAC(k, k1, k2);    
+    return KeyCMAC!blockSize(k1, k2);
 }
 
-auto getTagCMAC(alias blockCipher, int blockSize)(ref const(KeyCMAC!blockSize) key, const(ubyte)[] message)
+auto getTagCMAC(int blockSize)(scope BlockCipher!blockSize blockCipher, KeyCMAC!blockSize key, const(ubyte)[] message)
 {
     ubyte[blockSize] mLast = void;
     // complete block
-    if (message.length % blockSize == 0)
+    if (message.length && message.length % blockSize == 0)
         mLast[] = key.k1[] ^ message[$ - blockSize..$];
     // incomplete block
     else
@@ -93,16 +150,18 @@ auto getTagCMAC(alias blockCipher, int blockSize)(ref const(KeyCMAC!blockSize) k
         mLast[byteCount + 1..$] = key.k2[byteCount + 1..$];
     }
     
-    int messageIndex = 0;
+    size_t messageIndex = 0;
     ubyte[blockSize] x = 0;
     ubyte[blockSize] y = void;
-    do 
+
+    auto numIters = message.length / blockSize;
+    foreach (i; 0..numIters)
     {
         y[] = x[] ^ message[messageIndex..messageIndex + blockSize];
-        x = blockCipher(key.k, y[]);
+        x = blockCipher(y);
+        messageIndex += blockSize;
     }
-    while (messageIndex < message.length);
 
     y[] = mLast[] ^ x[];
-    return blockCipher(key.k, y[]);
+    return blockCipher(y);
 }
