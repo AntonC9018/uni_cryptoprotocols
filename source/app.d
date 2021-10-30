5165099998a08513adfa9d7645d40e3a444d41e7
AntonC9018 <anton.curm@gmail.com>
2021-10-30 22:38:45 +0300

    stuff

--- source/app.d
+++ source/app.d
@@ -299,8 +299,8 @@ template UMAC(size_t aesBlockLength = 16, size_t aesKeyLength = 128 / 8, size_t 
             ReturnType!l2Hash B = void;
             if (message.length <= l1KeySliceLength)
             {
-                B[0..A.length] = A[];
-                B[A.length..$] = 0;
+                B[0 .. A.length] = A[];
+                B[A.length .. $] = 0;
             }
             else
             {
@@ -319,29 +319,159 @@ template UMAC(size_t aesBlockLength = 16, size_t aesKeyLength = 128 / 8, size_t 
         return result;
     }
 
-    ubyte[] l1Hash(in ubyte[l1KeyLength] key, const(ubyte)[] message)
+    ulong[] l1Hash(in ubyte[l1KeyLength] key, const(ubyte)[] message)
     {
+        assert(message.length != 0);
         auto t = ceilingDivide(message.length, l1KeyLength);
-        ubyte[l1KeyLength] intermediateResult;
+
+        // ubyte[l1KeyLength] intermediateResult;
+        auto result = new ulong[](t);
+        ulong lenIdk = l1KeyLength * 8;
+        
         foreach (i; 0 .. (t - 1))
         {
             size_t messageIndexStart = i * l1KeyLength;
             ubyte[l1KeyLength] chunk = message[messageIndexStart .. messageIndexStart + l1KeyLength];
             maybeEndianSwap(cast(uint[]) chunk);
-            chunk = nh(key, chunk);
-            chunk = mod64add(chunk, 
+            ulong nextLong = nh(key, chunk) + lenIdk;
+            result[i] = nextLong;
+        }
+
+        {
+            lenIdk = message.length * 8;
+            ubyte[l1KeyLength] chunk = void;
+            size_t messageStartIndex = (t - 1) * l1KeyLength;
+            chunk[0 .. message.length - messageStartIndex] = message[messageStartIndex .. $];
+            chunk[message.length - messageStartIndex .. $] = 0;
+            maybeEndianSwap(cast(uint[]) chunk[]);
+            ulong lastLong = nh(key, chunk) + lenIdk;
+            result[t - 1] = lastLong;
+        }
+        return result;
+    }
+
+    ulong nh(in ubyte[l1KeyLength] key, in ubyte[l1KeyLength] message)
+    {
+        ulong result = 0;
+        auto key4 = cast(uint[l1KeyLength / 4]) key;
+        auto message4 = cast(uint[l1KeyLength / 4]) message4;
+        uint[l1KeyLength / 4] sums = key4[] + message4[];
+
+        // This can be easily refactored to use simd instructions.
+        foreach (i; iota(0, l1KeyLength / 4, 2))
+            result += cast(ulong) sums[i] * cast(ulong) sums[i + 1];
+
+        // return cast(ubyte[8]) &result[0..1];
+        return result;
+    }
+
+    ubyte[16] l2Hash(in byte[l2KeyLength] key, const(ubyte)[] message)
+    {
+        ulong mask64  = 0x01ffffff01ffffff;
+        ulong k64     = (cast(ulong[1]) key[0..8])[0] & mask64;
+        ulong[2] k128 = cast(ulong[2]) key[8..24];
+        enum p64 = 0xFFFFFFFF_FFFFFFC5;
+        k128[] &= mask64;
+
+        if (message.length <= 2^^17)
+        {
+            ulong y = poly64(cast(ulong) -2^^32, k64, p64); 
         }
     }
 
-    ubyte[8] nh(in ubyte[l1KeyLength] key, in ubyte[l1KeyLength] message)
+    // yoinked from https://github.com/openssh/openssh-portable/blob/0328a081f38c09d2d4d650e94461a47fb5eef536/umac.c#L797
+    // But I do not understand wth it even does.
+    ulong poly64(ulong current, ulong key, ulong data)
     {
-        ubyte[8] result;
-        foreach (i; 0 .. l1KeyLength / 4)
-        {
-            result.mod32add(message[i .. i + 4].mod32add(key[i .. i + 4])
-                .mod64mul(message[i + 4 .. i + 8].mod32add(key[i + 4 .. i + 8])));
-        }
+        ulong key_hi = cast(uint)(key >> 32);
+        ulong key_lo = cast(uint)(key);
+        ulong current_hi = cast(uint)(current >> 32);
+        ulong current_lo = cast(uint)(current);
+
+        ulong X = key_hi * current_lo + current_hi * key_lo;
+        ulong x_lo = cast(uint)(X);
+        ulong x_hi = cast(uint)(X >> 32);
+
+        const ulong mystery = 59;
+        ulong result = key_hi * current_hi + x_hi;
+        ulong result = result * mystery + key_lo * current_lo;
+        ulong T = x_lo << 32;
+
+        result += T;
+        if (result < T)
+            result += mystery;
+
+        result += data;
+        if (result < data)
+            result += mystery;
+
+        return result;
     }
+
+    // tried implementing this, but it's just too overwhelming
+    static if (0)
+    auto poly(ulong wordLength8)(ulong[wordLength8] maxwordrange, ulong[wordLength8] k, ulong[] message)
+    {
+        static assert(wordLength8 == 1 || wordLength8 == 2);
+        enum numBytes = wordLength8 * 8;
+
+        static if (wordLength8 == 1)
+        {
+            static immutable(ulong)[1] prime = [0xFFFFFFFF_FFFFFFC5];
+            static immutable(ulong)[1] offset = [-prime[0]]; 
+        }
+        else
+        {
+            static immutable(ulong)[2] prime = [0xFFFFFFFF_FFFFFF61, 0xFFFFFFFF_FFFFFFFF];
+            static immutable(ulong)[2] offset = [-prime[0], ~prime[1]];
+            assert((message.length & 1) == 0, "Message must be divisible by wordLength in multiples of 8 bytes.");
+        }
+        static immutable(ulong)[wordLength8] marker = prime[] - 1;
+
+        size_t numChunks = message.length / wordLength8;
+
+        ulong[wordLength8] result;
+        result[0] = 1;
+        foreach (i; iota(0, numChunks, wordLength8))
+        {
+            ulong[wordLength8] m = message[i .. i + wordLength8];
+            // Just check the most significant bytes
+            // Only check the other bytes if the most significat bytes were equal
+            bool isGreaterOrEqual()
+            {
+                foreach_reverse (index, component; m)
+                {
+                    // If a more significat bit is greater (smaller), all the rest don't matter
+                    if (component > maxwordrange[index])
+                        return true;
+                    if (component < maxwordrange[index])
+                        return false;
+                }
+                return true; // All were equal
+            }
+            if (isGreaterOrEqual)
+            {
+                // TODO: Implement 128 bits. It's not that simple.
+                // TODO: This simple thing I did won't work even for 64 bits, because it might overflow.
+                result[] = (k[] * result[] + marker[]) % prime[];
+                result[] = (k[] * result[] + (m[] - offset[])) % prime[];
+            }
+            else
+            {
+                result[] = (k[] * result[] + m[]) % prime[];
+            }
+        }
+        return result;
+    }
+
+    // ulong polyMultiply(ulong a, ulong b, ulong prime)
+    // {
+    //     ulong result = 1;
+    //     foreach (i; 0 .. ulong.sizeof * 8)
+    //     {
+    //         ulong currentBit = ((a >> i) & 1);
+    //     }
+    // }
 }
 
 
